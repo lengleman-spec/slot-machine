797c774b4d2471e813bf65e12ab078aa640c9273
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-04-12 15:27:59 -0400

    Create function to handle initial deposit amount.

--- project.js
+++ project.js
@@ -5,3 +5,20 @@
 // 5. Check if the user won
 // 6. Give the user their winnings
 // 7. Play again?
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount); // parseFloat takes a string and turns it into a number
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount. Try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const depositAmount = deposit();
