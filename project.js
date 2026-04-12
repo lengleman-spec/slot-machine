a081cb9ea6bbb1d788b5f925bc6b25e1fde09387
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-04-12 16:15:12 -0400

    Add logic to spin the slot machine.

--- project.js
+++ project.js
@@ -8,6 +8,23 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+
+const SYMBOL_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
+
 // #1
 const deposit = () => {
   while (true) {
@@ -50,6 +67,34 @@ const getBet = (balance, lines) => {
   }
 };
 
+// #4
+const spin = () => {
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    console.log(symbol, count);
+
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+
+  const reels = [[], [], []]; // Each array represents 1 column in slot machine
+  for (let i = 0; i < COLS; i++) {
+    const reelSymbols = [...symbols];
+
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+
+  return reels;
+};
+
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
