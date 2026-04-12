73261008366e62904af774b99ecc2c10817fe817
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-04-12 15:17:31 -0400

    Add javascript logic via comments.

---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. Deposit some money
+// 2. Determine # of lines to bet
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again?
