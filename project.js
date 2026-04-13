628345b59d75b84f41efa78070fca5738c31409a
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-04-13 15:10:20 -0400

    Add logic to display user balance and ask if user wants to play another round.

--- project.js
+++ project.js
@@ -145,11 +145,33 @@ const getWinnings = (rows, bet, lines) => {
   return winnings;
 };
 
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines);
-const reels = spin();
-const rows = transpose(reels);
-printRows(rows);
-const winnings = getWinnings(rows, bet, numberOfLines);
-console.log("You won, $" + winnings.toString());
+//#6
+const game = () => {
+  let balance = deposit();
+
+  while (true) {
+    console.log("You have a balance of $" + balance);
+    const numberOfLines = getNumberOfLines();
+    const bet = getBet(balance, numberOfLines);
+
+    balance -= bet * numberOfLines;
+    const reels = spin();
+    const rows = transpose(reels);
+    printRows(rows);
+    const winnings = getWinnings(rows, bet, numberOfLines);
+
+    balance += winnings;
+    console.log("You won, $" + winnings.toString());
+
+    if (balance <= 0) {
+      console.log("You ran out of money!");
+      break;
+    }
+
+    const playAgain = prompt("Do you want to play again? (y/n)");
+
+    if (playAgain != "y") break;
+  }
+};
+
+game();
